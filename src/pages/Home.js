6cc315d9026c9bf3e33f2bcdbb106ac909b328fc
Kevin O'Connell <kevinoconnell42@gmail.com>
2021-02-16 17:57:03 -0600

    responsive to Image element

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,16 +14,7 @@ export default function Home() {
  
  const heroImage = "https://lh3.googleusercontent.com/47G_daeEICHNPlrTKxPjZBHu5uV6uKr70HcCTm_oE0XlCt3RZxi3FPqRYonqRgd_ozSwbodo-jB44uzKwkmEUgtNKJtC2zYzV6CJPVHANSO3psVtxTBKuPH0-wOCGGZxm89V3pJZkElBUj9U-iaPJgjBXYQKXbAQCZP7jlLAq-5AYB6THZcv-LKKjMMwktfa7vFOYg69SJPUhANLlgmhZwWYR99d-ocoGip9jiOYrCtoPJJIaK4giV4IQK5K0tXLLMrcFMOguVJkqnQGF-9Cm4piUHwFn9jRc3Gh_g4hY4HtVC7unmbBp3Dyf38KEAu26Vekoy2M07U9sHtTrmdY1apWiUhZctMsLVycpJRHGNimrZi5HwQEwfTIWwNSToIXyQnCeQA51imI2KdJimU-Ip0KYCtGpevkeyQ98iUrn-c0_xAzv-6PXlYUogPEiWYxXmkB-vS6tQY3pc0EHAXJv92K7_sEYjkHOpgtjqIvfLol_Lz29unr6Ektm9YhvgZEqkoAUqgNM8QwrLWj0gPe-Midab1cLefSqudc6NmPueYvKvVKYU22UaqjdL8iCMA432omkE21CcU_Gn9LeLJ1wA6o7YopLvTjI1_lbxgT71fn52g6tjUKBOdaxIBnsmggekm39f27GnZBAHsx4cZQ0ncFmjU2w5vtiPHsOeiT3e52qTEY10eVEMKz_u93Qw=w600-h1299-no?authuser=0"
     let waitListMembers = 12;
-
-const Desktop = ({ children }) => {
-  const isDesktop = useMediaQuery({ minWidth: "601px" })
-  return isDesktop ? children : null
-}
-const Mobile = ({ children }) => {
-
-  const isMobile = useMediaQuery({ maxWidth: "600px" })
-  return isMobile ? children : null
-}
+const isMobile = useMediaQuery({ query: '(max-width: 600px)' })
     return (
         <div>
             <div className="hero">
@@ -68,8 +59,7 @@ const Mobile = ({ children }) => {
             <section className="built-by">
                 <div style={{ display: "inline-block" }}>
                 <div style={{ display: "inline-block", verticalAlign: "middle" }}>
-                    <Mobile><Image src={me} roundedCircle width = "75" height = "75" /> </Mobile>
-                    <Desktop><Image src={me} roundedCircle width = "175" height = "175" /></Desktop>
+                    <Image src={me} roundedCircle responsive width = "175" height = "175" />
                 </div>
 
                 <div style={{ display: "inline-block" }} className="built-by-details">
